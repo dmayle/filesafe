245ccbb23df88d872e9d90c14d7748c43a545b73
Douglas Mayle http://douglas.mayle.org <devnull@localhost>
2009-03-31 15:18:16 -0400

    expand to support file sanitization

--- filesafe/__init__.py
+++ filesafe/__init__.py
@@ -1,17 +1,41 @@
 #
 from os import path
+from re import compile
+
+INVALID_FILE_CHARS = compile(r'[?%*:|"<>/]')
+INVALID_DIR_CHARS = compile(r'[?%*:|"<>]')
+filetypes = {'dir': INVALID_DIR_CHARS,
+             'file': INVALID_FILE_CHARS}
 
 class Chroot(object):
-    def __init__(self, chrootpath):
-        # For safety reasons, the chroot path must start with a seperator to remove directory confusion
-        self.chroot = path.abspath(chrootpath) + path.sep
+    def __init__(self, chrootpath, sanitize_method=None):
+        if sanitize_method not in [None, 'strip', 'encode']:
+            sanitize_method = None
+        self.sanitize_method = sanitize_method
+
+        # For safety reasons, the chroot path must start with a seperator to
+        # remove directory confusion. e.g. /etc and /etc2
+        self.chroot = path.abspath(self.__sanitize('dir', chrootpath)) + path.sep
+
         if not path.isdir(self.chroot):
-            raise 'Some Error'
+            raise IOError
+
+    def __sanitize(self, filetype, dirname):
+        if not self.sanitize_method:
+            if filetypes[filetype].search(dirname):
+                raise IOError
+            return dirname
+        raise NotImplementedError('The only sanitization method currently supported is none')
+
     def __call__(self, filepath):
-        filechroot = path.abspath(filepath)
-        # Needs to worry about similar names
+        filechroot = path.abspath(self.__sanitize('dir', filepath))
+
+        # Test
         if not filechroot.startswith(self.chroot):
             raise IOError
         if filechroot == self.chroot:
             raise IOError
-        return filepath
+
+        # Return the modified version.  We should change this to edit the
+        # passed in variable.  Less confusion
+        return filechroot
--- tests/__init__.py
+++ tests/__init__.py
@@ -2,12 +2,21 @@ import unittest
 from filesafe import Chroot
 
 class ChrootTests(unittest.TestCase):
+
     def test_chroot(self):
+        # Bad test, it might exist.  The best I can think of is to make sure it
+        # doesn't exist before and after, but that has race conditions.
+        self.assertRaises(IOError, Chroot, '/ajfljalkjflkasjflkas')
+
+    def test_chroot_operations(self):
         etc = Chroot('/etc')
-        self.assertEquals(etc('/etc/filename'), '/etc/filename', 'File exists in chroot')
+        self.assertEquals(etc('/etc/filename'), '/etc/filename', 'File does not exist in chroot')
         self.assertRaises(IOError, etc, '/filename')
         self.assertRaises(IOError, etc, '/etc2')
         self.assertRaises(IOError, etc, '/etc')
+        self.assertRaises(IOError, etc, '/etc/../root')
+        self.assertEquals(etc('/etc/dir/../filename'), '/etc/filename', 'File does not exist in chroot')
+        self.assertRaises(IOError, etc, '/etc/file?name')
 
 if __name__ == '__main__':
     unittest.main()
